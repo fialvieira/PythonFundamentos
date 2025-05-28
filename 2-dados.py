f7e3dc8157d88da01dbf20d62975e8a959d5469d
fialvieira <fialvieira@gmail.com>
2025-05-28 14:36:48 -0300

    Ajustes no arquivo 2-dados

---
+++ 2-dados.py
@@ -0,0 +1,13 @@
+# PythonFlix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+movieRating = 9.5
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(movieRating))
+print(type(planIncluded))
